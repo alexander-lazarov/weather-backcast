1ba045b56379a46507954ce470467b77675acf7c
Alexander Lazarov <alexander.lazaroff@gmail.com>
2015-11-20 21:19:32 +0200

    create measurements table and model

---
+++ database/migrations/2015_11_20_190915_create_measurements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMeasurementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('measeurements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('spot_id')->index();
+            $table->decimal('wind_speed', 3, 1);
+            $table->decimal('wind_gust_speed', 3, 1);
+            $table->decimal('wind_direction', 3, 0);
+            $table->decimal('temperature', 3, 1);
+            $table->decimal('pressure', 5, 1);
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('measeurements');
+    }
+}
